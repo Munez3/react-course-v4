a330b5effc08953f4157b3464fc3db7b87b9f200
Munez3 <melwin03@gmail.com>
2023-05-25 18:37:37 +0200

    exercise 19-21

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./16_router/App";
-import Router from "./18_context_router/Router";
+import Router from "./21_useEffect/Router";
 import ParamTest from "./16_router/ParamTest";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

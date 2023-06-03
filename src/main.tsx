2e5d41b8418cc333a31337cca7303d2603501bf0
Munez3 <melwin03@gmail.com>
2023-06-03 10:09:15 +0200

    pdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./16_router/App";
+import App from "./2_props/App";
 import Router from "./21.2_useEffect/Router";
 import ParamTest from "./16_router/ParamTest";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Router />
+    <App />
+    {/* <Router /> */}
     {/* <ParamTest /> */}
   </React.StrictMode>
 );
